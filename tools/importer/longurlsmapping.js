84684f5184afe5274be7c6b2cd7bec72bf0c1a3b
hsaginor <hsaginor@gmail.com>
2023-12-12 22:47:01 -0800

    filter out landing pages

--- tools/importer/longurlsmapping.js
+++ tools/importer/longurlsmapping.js
@@ -1,6 +1,10 @@
 import fetch from '@adobe/node-fetch-retry';
 import { JSDOM } from 'jsdom';
 
+const unsupportedSelectors = [
+    'body.homePage', 'body.landingPage'
+]
+
 async function parseHTML(text) {
     const jsdom = new JSDOM(text);
     return jsdom.window.document;
@@ -15,26 +19,40 @@ async function fetchDocument(url) {
       console.log(`Unable to fetch ${url}. Response status: ${resp.status}`);
     }
 }
-  
+
+function filterPageTypes(doc) {
+    unsupportedSelectors.forEach((sel) => {
+        const el = doc.querySelector(sel);
+        if (el) {
+            throw new Error(`Unsapported page type selector '${sel}'`);
+        }
+    });
+}
+
 async function fetchLongPath(url) {
     // console.log(`fetching long path from ${url}`);
     const doc = await fetchDocument(url);
     if (doc) {
+        filterPageTypes(doc);
         return doc.body.getAttribute('data-page-path');
     }
 }
 
 const map = async (orgUrl, params) => {
-    const longPath = await fetchLongPath(orgUrl); 
-    if (longPath) {
-        try {
-            const url = new URL(orgUrl);
-            return `${url.protocol}//${url.hostname}${longPath}`
-        } catch (err) {
-            console.log(`Unable to get long path for ${orgUrl}. ${err.message}`);
+    try {
+        const longPath = await fetchLongPath(orgUrl); 
+        if (longPath) {
+            try {
+                const url = new URL(orgUrl);
+                return `${url.protocol}//${url.hostname}${longPath}`
+            } catch (err) {
+                console.log(`Unable to get long path for ${orgUrl}. ${err.message}`);
+            }
         }
+        return orgUrl;
+    } catch (err) {
+        console.error(`${orgUrl}: ${err.message}`);
     }
-    return orgUrl;
 }
 
 export {
